442e6a9ff382be6ed9e37b0938988a5e37aebdd3
monicagangwar <monicagangwar10@gmail.com>
2017-01-05 18:01:34 +0530

    Giving circle iam user full access to codedeploy

--- circle-iam-user.tf
+++ circle-iam-user.tf
@@ -13,6 +13,16 @@ resource "template_file" "s3_w"{
         bucket_name = "${aws_s3_bucket.S3_BUCKET.bucket}"
     }
 }
+resource "template_file" "codedeploy_full_access"{
+    filename = "${path.module}/templates/codedeploy_full_access.tpl"
+}
+
+resource "aws_iam_user_policy" "codedeploy_full_access"{
+    name = "codedeploy_full_access"
+    user = "${aws_iam_user.circleci.name}"
+    policy = "${template_file.codedeploy_full_access.rendered}"
+}
+
 resource "aws_iam_user_policy" "s3_w" {
     name = "s3_w"
     user = "${aws_iam_user.circleci.name}"
