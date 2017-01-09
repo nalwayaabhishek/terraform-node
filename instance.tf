61105d904a5f8adbaa33c063cbc16f96652f2bf9
monicagangwar <monicagangwar10@gmail.com>
2017-01-09 14:39:58 +0530

    Adding s3 access iam profile to ec2 instance

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "node-instance" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.EC2-S3-Full-Access.name}"
 
   # the public SSH key
   key_name = "${aws_key_pair.mykeypair.key_name}"
