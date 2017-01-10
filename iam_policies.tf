52198bf8efadc0835a35ee3ff90708225c9d0be5
monicagangwar <monicagangwar10@gmail.com>
2017-01-10 19:08:44 +0530

    Fixing policy parsing error

--- iam_policies.tf
+++ iam_policies.tf
@@ -1,15 +1,15 @@
 data "template_file" "s3_full_access"{
-    template = "${path.module}/templates/s3_full_access.tpl"
+    template = "${file("${path.module}/templates/s3_full_access.tpl")}"
     vars{
         bucket_name = "${aws_s3_bucket.S3_BUCKET.bucket}"
     }
 }
 data "template_file" "codedeploy_full_access"{
-    template = "${path.module}/templates/codedeploy_full_access.tpl"
+    template = "${file("${path.module}/templates/codedeploy_full_access.tpl")}"
 }
 
 data "template_file" "ec2_trust_policy"{
-    template = "${path.module}/templates/ec2_trust_policy.tpl"
+    template = "${file("${path.module}/templates/ec2_trust_policy.tpl")}"
 }
 resource "aws_iam_policy" "s3_full_access" {
   name = "s3_full_access"
--- templates/s3_full_access.tpl
+++ templates/s3_full_access.tpl
@@ -1,8 +1,9 @@
-{"Version": "2012-10-17",
+{
+  "Version": "2012-10-17",
   "Statement": [
     {
-      "Effect": "Allow",
       "Action": "s3:*",
+      "Effect": "Allow",
       "Resource": "*"
     }
   ]
