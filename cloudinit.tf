e2e6ef753f9208a3b2a4b96630ac94658159088b
Monica Gangwar <monicagangwar10@gmail.com>
2017-01-10 12:23:57 +0530

    Refactored the code (#9)
    * Adding s3 access iam profile to ec2 instance
    
    * Refactoring
    
    * Refactored the code

--- cloudinit.tf
+++ cloudinit.tf
@@ -4,7 +4,7 @@ data "template_file" "init-script" {
   template = "${file("templates/init.cfg")}"
   vars {
     userdata_index = "${base64encode(file("${path.module}/files/index.html"))}"
-    frontent_nginx_conf = "${base64encode(file("${path.module}/files/frontent.conf"))}"
+    nginx_conf = "${base64encode(file("${path.module}/files/nginx.conf"))}"
     backend_tmp_app = "${base64encode(file("${path.module}/files/backend/app.js"))}"
     backend_tmp_app_package = "${base64encode(file("${path.module}/files/backend/package.json"))}"
     pm2_conf = "${base64encode(file("${path.module}/templates/processes.tpl"))}"
--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "node-instance" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_s3_full_access.name}"
 
   # the public SSH key
   key_name = "${aws_key_pair.mykeypair.key_name}"
